e1a3a352e5e653c5348cc5a98afc76e37c98d585
Alexandre <alexandre@tfo.com.br>
2022-12-17 15:18:18 -0300

    Limpando o exemplo de JQuery

--- Samples.js
+++ Samples.js
@@ -9,15 +9,8 @@ function StartupScripts() {
 
     //aqui coloca os jquery que precisam ser inicializado pelo "$(document).ready"
 
-     > var a = NaN;
-    > a === a
-    false
-        > var b = {}, c = {};
-    > b === c
-    false
-        > b === b
-    true
-        > "abc" === new String("abc")
-    false // different types (left: primitive, right: object)
+   
 
 }
+
+
