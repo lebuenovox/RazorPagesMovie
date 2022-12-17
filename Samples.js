99d121c2b7e1de612d71f44fd877487059daf8fd
Alexandre <alexandre@tfo.com.br>
2022-12-17 15:00:40 -0300

    subindo commit com problema

--- Samples.js
+++ Samples.js
@@ -2,11 +2,4 @@ $(document).ready(function () {
     //faz referencia a uma function de Startup no arquivo 
     StartupScripts();
 
-    //////
-    ///teste
 });
-
-//no arquivo *.js
-function StartupScripts() {
-
-}
